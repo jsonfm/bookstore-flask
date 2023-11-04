5de0c8e54a6a0779e3453acba274f7be132d014d
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2023-11-04 16:23:50 -0500

    ✨ [UPDATE] login form

--- app/routers/api/auth.py
+++ app/routers/api/auth.py
@@ -1,12 +1,19 @@
 from flask import Blueprint
+from flask_pydantic import validate
 
+# foms
+from app.schemas.auth.forms import LoginForm
+
+# utils
 from app.utils.auth import hash_password
 
 router = Blueprint("auth", __name__, url_prefix="/auth")
 
 
 @router.route("/login", methods=["POST"])
-def login():
+@validate()
+def login(body: LoginForm):
+    print("login: ", body.dict())
     return "login"
 
 
