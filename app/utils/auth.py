0145d6f532ddb7df82ed4d29dac0cc097b6d6360
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2023-11-04 12:11:59 -0500

    ⚡️[UPDATE] auth utils

---
+++ app/utils/auth.py
@@ -0,0 +1,14 @@
+# crypto
+from passlib.context import CryptContext
+from passlib.hash import pbkdf2_sha256
+
+#
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def hash_password(password: str):
+    return pbkdf2_sha256.hash(password)
+
+
+def verify_password(password: str, hashed: str):
+    return pbkdf2_sha256.verify(password, hashed)
